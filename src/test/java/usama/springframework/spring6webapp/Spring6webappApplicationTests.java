ac232b2673e9194fb6ef9f3157d88626442d8641
usama-unitydeveloper <2018ce214@student.uet.edu.pk>
2023-04-16 16:51:30 +0500

    First Commit

---
+++ src/test/java/usama/springframework/spring6webapp/Spring6webappApplicationTests.java
@@ -0,0 +1,13 @@
+package usama.springframework.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
