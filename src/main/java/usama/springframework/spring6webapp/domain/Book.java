a898b38df04521b8386fda1c7f56ab0826f7d755
usama-unitydeveloper <2018ce214@student.uet.edu.pk>
2023-04-16 19:48:37 +0500

    Required Entries added2

--- src/main/java/usama/springframework/spring6webapp/domain/Book.java
+++ src/main/java/usama/springframework/spring6webapp/domain/Book.java
@@ -12,4 +12,28 @@ public class Book {
     private Long id;
     private String title;
     private String isBin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsBin() {
+        return isBin;
+    }
+
+    public void setIsBin(String isBin) {
+        this.isBin = isBin;
+    }
 }
