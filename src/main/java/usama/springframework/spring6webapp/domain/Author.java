9f19a56f266fd57f8bad4770864385de7b7a4c48
usama-unitydeveloper <2018ce214@student.uet.edu.pk>
2023-04-16 19:43:38 +0500

    Required Entries added2

--- src/main/java/usama/springframework/spring6webapp/domain/Author.java
+++ src/main/java/usama/springframework/spring6webapp/domain/Author.java
@@ -1,6 +1,14 @@
 package usama.springframework.spring6webapp.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
--- src/main/java/usama/springframework/spring6webapp/domain/Book.java
+++ src/main/java/usama/springframework/spring6webapp/domain/Book.java
@@ -1,6 +1,14 @@
 package usama.springframework.spring6webapp.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String isBin;
