fa9a3670c5c4ec4de3a5fef6b9f41b8a0ae54c53
usama-unitydeveloper <2018ce214@student.uet.edu.pk>
2023-04-16 17:08:31 +0500

    Required Entries added

---
+++ src/main/java/usama/springframework/spring6webapp/domain/Author.java
@@ -0,0 +1,8 @@
+package usama.springframework.spring6webapp.domain;
+
+public class Author {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/main/java/usama/springframework/spring6webapp/domain/Book.java
@@ -0,0 +1,7 @@
+package usama.springframework.spring6webapp.domain;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String isBin;
+}
